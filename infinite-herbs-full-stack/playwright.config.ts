8f9be794384ef81b07e2e270459e10249ccd14d9
jhusseth1 <jhussethsanchez@gmail.com>
2026-03-04 10:58:20 -0500

    feat: fix test

--- infinite-herbs-full-stack/playwright.config.ts
+++ infinite-herbs-full-stack/playwright.config.ts
@@ -1,27 +1,21 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
+
+import dotenv from 'dotenv';
+import path from 'path';
+
+const ENV = process.env.ENV ?? 'test';
+dotenv.config({path: path.resolve(__dirname , `.env.${ENV}`)});
 
 export default defineConfig({
-  testDir: './e2e',
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: 3,
-  reporter: 'html',
-  
+  testDir: './e2e' ,
   use: {
-    baseURL: 'http://localhost:3000',
-    trace: 'on-first-retry',
-  },
-
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-  ],
+    baseURL: 'http://localhost:3000' ,
+    trace: 'on-first-retry' ,
+  } ,
   webServer: {
-    command: 'npm run dev',
-    port: 3000,
-    reuseExistingServer: !process.env.CI,
-  },
+    command: 'npm run dev' ,
+    url: 'http://localhost:3000' ,
+    reuseExistingServer: !process.env.CI ,
+    timeout: 120000
+  }
 });
