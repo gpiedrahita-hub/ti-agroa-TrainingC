4169e3743effa0023204d326d336fa068c97e6bd
jhusseth1 <jhussethsanchez@gmail.com>
2026-03-04 10:37:44 -0500

    feat: update eslint config

--- infinite-herbs-frontend/eslint.config.mjs
+++ infinite-herbs-frontend/eslint.config.mjs
@@ -1,23 +1,22 @@
-import {defineConfig, globalIgnores} from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
 
 const eslintConfig = defineConfig([
-    ...nextVitals,
-    ...nextTs,
-    // Override default ignores of eslint-config-next.
-    globalIgnores([
-        // Default ignores of eslint-config-next:
-        ".next/**",
-        "out/**",
-        "build/**",
-        "next-env.d.ts",
-    ]),
-    module.exports = {
-        rules: {
-            'react-hooks/set-state-in-effect': 'off',
-        }
-    }
+  ...nextVitals,
+  {
+    rules: {
+        'react-hooks/set-state-in-effect': 'off',
+    },
+  },
+  
+  globalIgnores([
+    '.next/**',
+    'out/**',
+    'dist/**',
+    'build/**',
+    'next-env.d.ts',
+    '*.config.js',
+  ]),
 ]);
 
 export default eslintConfig;
