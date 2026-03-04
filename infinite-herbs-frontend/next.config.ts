94d0b8ce03b7a9d74c4b56b186618b73c979329d
jhusseth1 <jhussethsanchez@gmail.com>
2026-03-04 10:52:41 -0500

    feat: fix test

--- infinite-herbs-frontend/next.config.ts
+++ infinite-herbs-frontend/next.config.ts
@@ -6,8 +6,6 @@ const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
-  basePath: '',
-  assetPrefix: './_next/static',
   distDir: 'dist',
 };
 
--- infinite-herbs-full-stack/playwright.config.ts
+++ infinite-herbs-full-stack/playwright.config.ts
@@ -1,21 +1,27 @@
-import { defineConfig } from '@playwright/test'
-
-import dotenv from 'dotenv'
-import path from 'path'
-
-const ENV = process.env.ENV ?? 'test'
-dotenv.config({ path: path.resolve(__dirname, `.env.${ENV}`) })
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: 3,
+  reporter: 'html',
+  
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
   },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
   webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:3000',
+    port: 3000,
     reuseExistingServer: !process.env.CI,
-    timeout: 120000
-  }
-})
+  },
+});
